0007672aab25ea7c8013ca5c5361baa1048786d8
iOS Notları <erayalparslan@yahoo.com>
2018-09-29 21:42:11 +0300

    Add files via upload
    iOS Notları

---
+++ Tut_GoBack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tut_GoBack
+//
+//  Created by ruroot on 9/5/18.
+//  Copyright © 2018 Eray Alparslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("iOS Notları'ndan merhaba!")
+    }
+
+  
+    @IBAction func unwindToFirst(_segue: UIStoryboardSegue){
+        
+    }
+
+
+}
+
